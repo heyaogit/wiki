99592c9898ac81a8f7a7148fb6d77b4035adf01c
heyaogit <384415026@qq.com>
2022-10-01 14:18:22 +0800

    接口的讲解和Componentsscan的包范围

---
+++ src/main/java/com/example/wikiproject/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.wikiproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello_world";
+    }
+}
