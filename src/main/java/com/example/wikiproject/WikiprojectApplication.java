21550ba81dbd42507f4046b18ce77a644f8057f2
heyaogit <384415026@qq.com>
2022-10-01 13:43:44 +0800

    修改启动文案 日志文件优化

--- src/main/java/com/example/wikiproject/WikiprojectApplication.java
+++ src/main/java/com/example/wikiproject/WikiprojectApplication.java
@@ -1,13 +1,20 @@
 package com.example.wikiproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiprojectApplication {
+    private static final Logger LOG= LoggerFactory.getLogger(WikiprojectApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiprojectApplication.class, args);
+        SpringApplication app=new SpringApplication(WikiprojectApplication.class);
+        Environment env=app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址:\thttp127.0.0.1:{}",env.getProperty("server.port") );
     }
 
 }
