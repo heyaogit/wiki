85f4a30632886192a329f5187cf9cb64eba098d4
heyaogit <384415026@qq.com>
2022-10-01 16:44:51 +0800

    配置项和Value的优先级读取 配置项优先

--- src/main/java/com/example/wikiproject/controller/TestController.java
+++ src/main/java/com/example/wikiproject/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wikiproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:newpractice}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
 
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 
     @PostMapping("/hello/post")
