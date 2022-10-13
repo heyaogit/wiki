e4f59bb552b2f68ed9fdcc2406b2e0f6d278a51d
heyaogit <384415026@qq.com>
2022-10-13 10:11:43 +0800

    4-4使用vue cl生成web项目

--- src/main/java/com/example/wikiproject/service/EbookService.java
+++ src/main/java/com/example/wikiproject/service/EbookService.java
@@ -6,11 +6,10 @@ import com.example.wikiproject.domain.EbookExample;
 import com.example.wikiproject.mapper.EbookMapper;
 import com.example.wikiproject.req.EbookReq;
 import com.example.wikiproject.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.example.wikiproject.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,14 +21,17 @@ public class EbookService {
        EbookExample.Criteria criteria=ebookExample.createCriteria();
        criteria.andNameLike("%"+req.getName()+"%");
        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-       List<EbookResp>respList=new ArrayList<>();
-       for (Ebook ebook:ebookList){
-           EbookResp ebookResp=new EbookResp();
-         //  ebookResp.setId(ebook.getId());
-           BeanUtils.copyProperties(ebook,ebookResp);
-           //ebookResp.setId(123456L);
-           respList.add(ebookResp);
-       }
-       return respList;
+   //    List<EbookResp>respList=new ArrayList<>();
+//       for (Ebook ebook:ebookList){
+//           EbookResp ebookResp=new EbookResp();
+//         //  ebookResp.setId(ebook.getId());
+//           BeanUtils.copyProperties(ebook,ebookResp);
+      // EbookResp ebookResp=CopyUtil.copy(ebook,EbookResp.class);
+//           //ebookResp.setId(123456L);
+//           respList.add(ebookResp);
+//       }
+
+       List<EbookResp>list= CopyUtil.copyList(ebookList,EbookResp.class);
+       return list;
    }
 }
