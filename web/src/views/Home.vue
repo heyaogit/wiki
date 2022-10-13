456058768ae57773c9aef74421dbdeda15342a34
heyaogit <384415026@qq.com>
2022-10-13 11:28:38 +0800

    4-5集成安ant design vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
+     <a-button type="text">Text Button</a-button>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
+
+
 
 <script lang="ts">
 import { defineComponent } from 'vue';
